5a7ef8328db2042fcc9405fe9e2f97ea5ef71637
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-02-03 18:33:33 +0100

    add annotation DTO

---
+++ src/main/java/tla/domain/model/dto/AnnotationDto.java
@@ -0,0 +1,16 @@
+package tla.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class AnnotationDto extends DocumentDto {
+
+    private String corpus;
+
+}
---
+++ src/test/java/tla/domain/model/dto/AnnotationTest.java
@@ -0,0 +1,35 @@
+package tla.domain.model.dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+public class AnnotationTest {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private AnnotationDto loadFromFile(String filename) throws Exception {
+        return mapper.readValue(
+            new File(
+                String.format("src/test/resources/sample/annotation/%s", filename)
+            ),
+            AnnotationDto.class
+        );
+    }
+
+    @Test
+    void deserializeFromFile() throws Exception {
+        AnnotationDto a = loadFromFile("5YUS7W6LUNC7DLP6SEIQDDKJHE.json");
+        assertAll("should be able to deserialize annotation",
+            () -> assertTrue(a != null, "should not be null"),
+            () -> assertEquals("leipzig_wlist", a.getCorpus(), "corpus value should be set"),
+            () -> assertEquals("annotation.lemma", a.getPassport().extractPaths().get(0), "lemma annotation field should exist in passport")
+        );
+
+    }
+}
