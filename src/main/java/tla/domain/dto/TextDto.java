40c46bcdfe6696fc784ebc27e746281c5fb94a86
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-03-14 12:54:15 +0100

    test text deserialization

--- src/main/java/tla/domain/dto/TextDto.java
+++ src/main/java/tla/domain/dto/TextDto.java
@@ -1,5 +1,10 @@
 package tla.domain.dto;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,5 +21,12 @@ public class TextDto extends DocumentDto {
 
     private String corpus;
     private Paths paths;
+
+    @JsonAlias("sentences")
+    private List<String> sentenceIds;
+
+    public TextDto() {
+        this.sentenceIds = Collections.emptyList();
+    }
 
 }
--- src/test/java/tla/domain/dto/TextTest.java
+++ src/test/java/tla/domain/dto/TextTest.java
@@ -3,14 +3,53 @@ package tla.domain.dto;
 import org.junit.jupiter.api.Test;
 
 import tla.domain.model.ObjectReference;
+import tla.domain.model.Passport;
 import tla.domain.model.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TextTest {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private TextDto loadFromFile(String filename) throws Exception {
+        return mapper.readValue(
+            new File(
+                String.format("src/test/resources/sample/text/%s", filename)
+            ),
+            TextDto.class
+        );
+    }
+
+
+    @Test
+    void deserializeFromFile() throws Exception {
+        TextDto t = loadFromFile("A2HWO5CX6RCQ5IBSV2MHAAIYA4.json");
+        assertAll("deserialization from file should preserve all contents",
+            () -> assertEquals("tlademotic", t.getCorpus()),
+            () -> assertTrue(t.getPaths() != null, "corpus object paths should be deserialized"),
+            () -> assertEquals(1, t.getPaths().size(), "expect exactly 1 path"),
+            () -> assertEquals(6, t.getPaths().get(0).size(), "count path segments"),
+            () -> assertTrue(t.getSentenceIds() != null, "sentence IDs should be preserved"),
+            () -> assertEquals(2, t.getSentenceIds().size(), "count sentences")
+        );
+        Passport pp = t.getPassport();
+        List<Passport> leafNodes = pp.extractProperty("object.description_of_object.type");
+        assertAll("passport contents should be preserved",
+            () -> assertNotNull(pp, "expect passport"),
+            () -> assertEquals(1, leafNodes.size(), "expect exactly 1 value for property path `object.description_of_object.type`"),
+            () -> assertTrue(leafNodes.get(0).get() instanceof ObjectReference, "leaf node should be thesaurus entry reference"),
+            () -> assertEquals("Mumienetikett", ((ObjectReference)leafNodes.get(0).get()).getName(), "check ths ref leaf node name")
+        );
+    }
+
 
     @Test
     void textBuilder() {
