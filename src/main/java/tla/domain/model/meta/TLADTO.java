fa359320c95a714eb215e464356b69a18bf7e6e3
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-03-31 22:39:48 +0200

    add @TLADTO annotation for client model classes

---
+++ src/main/java/tla/domain/model/meta/TLADTO.java
@@ -0,0 +1,20 @@
+package tla.domain.model.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import tla.domain.dto.DocumentDto;
+
+/**
+ * Use this on your client code's model classes in order to assign
+ * a corresponding DTO type to them.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TLADTO {
+
+    public Class<? extends DocumentDto> value();
+    
+}
