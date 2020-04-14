3e941d21e1154421faa66a0163c7ca8a005c84d3
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-04-14 16:54:57 +0200

    provide no arguments constructor

--- src/main/java/tla/domain/dto/CorpusObjectDto.java
+++ src/main/java/tla/domain/dto/CorpusObjectDto.java
@@ -2,12 +2,14 @@ package tla.domain.dto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import tla.domain.model.Paths;
 import tla.domain.model.meta.BTSeClass;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @BTSeClass("BTSTCObject")
 @EqualsAndHashCode(callSuper = true)
 public class CorpusObjectDto extends DocumentDto {
