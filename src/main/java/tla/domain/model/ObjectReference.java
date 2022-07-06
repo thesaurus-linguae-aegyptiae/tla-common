f10632899718d2d756e0d8dc08c5160d3de4a346
cvertan <cristina.vertan@uni-hamburg.de>
2022-07-06 11:48:00 +0200

    Corrected 2 Constructors in Object Reference

--- src/main/java/tla/domain/model/ObjectReference.java
+++ src/main/java/tla/domain/model/ObjectReference.java
@@ -103,25 +103,7 @@ public class ObjectReference implements Comparable<Resolvable>, Resolvable {
         this.ranges = ranges;
     }
 
-    @JsonCreator
-    public ObjectReference(
-        @JsonProperty(value = "id", required = false) String id,
-        @JsonProperty(value = "eclass", required = true) String eclass,
-        @JsonProperty(value = "type", required = false) String type,
-        @JsonProperty(value = "name", required = false) String name,
-        @JsonProperty(value = "ranges", required = false) List<Range> ranges
-    ) {
-        if(!id.isEmpty())this.id = id;
-        else this.id="composed";
-        System.out.println("ID constructor "+this.id);
-        this._class = _class;
-        this.eclass = eclass;
-        this.type = type;
-        this.name = name;
-        this.pos=pos;
-        this.variants=variants;
-        this.ranges = ranges;
-    }
+
 
     @Override
     public int compareTo(Resolvable arg0) {
@@ -166,16 +148,19 @@ public class ObjectReference implements Comparable<Resolvable>, Resolvable {
     	 if (object instanceof NamedDocumentDto) {
             return new ObjectReference(
                 object.getId(),
+                null,
                 object.getEclass(),
                 ((NamedDocumentDto) object).getType(),
                 ((NamedDocumentDto) object).getName(),
+                -1,-1,
               null
             );
         } else {
             return new ObjectReference(
                 object.getId(),
+                null,
                 object.getEclass(),
-                null,null,null
+                null,null,-1,-1,null
             );
         }
     }
