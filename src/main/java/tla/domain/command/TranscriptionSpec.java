bc9ba2914416b73e5be061280b180b6085c9fdaf
cvertan <cristina.vertan@uni-hamburg.de>
2022-08-30 21:51:36 +0200

    Canged NULL requirements for RootSpec and TranslationSPec

--- src/main/java/tla/domain/command/TranscriptionSpec.java
+++ src/main/java/tla/domain/command/TranscriptionSpec.java
@@ -20,7 +20,7 @@ public class TranscriptionSpec {
 
     @JsonIgnore
     public boolean isEmpty() {
-        return this.text == null && (this.enc == null || this.enc.length < 1);
+        return this.text == null || (this.enc == null || this.enc.length < 1);
     }
 
     public static class EmptyObjectFilter {
