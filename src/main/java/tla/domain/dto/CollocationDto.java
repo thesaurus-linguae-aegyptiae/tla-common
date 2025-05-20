fcda5a85d8b0932f26b1c6cdf359d2b14605f270
RDBloese <38698852+RDBloese@users.noreply.github.com>
2025-05-20 11:56:28 +0200

    Create CollocationDto.java

---
+++ src/main/java/tla/domain/dto/CollocationDto.java
@@ -0,0 +1,32 @@
+package tla.domain.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import tla.domain.dto.meta.AbstractDto;
+import tla.domain.model.meta.BTSeClass;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@BTSeClass("BTSCollocation") // TODO
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CollocationDto extends AbstractDto {
+
+	List<SentenceDto> sentences = new ArrayList<SentenceDto>();
+	List<String> tokenIds = new ArrayList<String>();
+	int distance;
+
+	public CollocationDto(List<SentenceDto> sentences, List<String> tokenIds, int distance) {
+		this.sentences = sentences;
+		this.tokenIds = tokenIds;
+		this.distance = distance;
+	}
+}
